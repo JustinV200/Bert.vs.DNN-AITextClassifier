e2098f4a13f9905d01117e880dc38bd7c0784886
JustinV200 <justinverlin@gmail.com>
2025-07-27 21:36:41 -0400

    debug

--- app.py
+++ app.py
@@ -10,6 +10,8 @@ import torch.nn.functional as F
 import os
 from flask import Flask, request, render_template
 from flask_cors import CORS
+#debugging
+print("Starting app...")
 
 app = Flask(__name__)
 CORS(app)
